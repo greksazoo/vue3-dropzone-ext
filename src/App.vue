daeeb068fd376a3b36a1a439e21b615f6c74889e
Zoltan Greksa <zoli.greksa@gmail.com>
2024-03-20 21:23:24 +0100

    update some

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <Vue3Dropzone multiple v-model="files">
+    </Vue3Dropzone>
+  </div>
+</template>
+
+<script setup>
+import Vue3Dropzone from "./components/Vue3Dropzone.vue";
+import {ref} from "vue";
+
+const files = ref([])
+</script>
+
+<style lang="scss" scoped>
+</style>
